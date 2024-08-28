65cc8ee2b04f4abe75713a49c8ee8a2b43573f6c
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-08-28 17:05:56 -0400

    moved spots.js file

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const sessionRouter = require("./session.js");
 const usersRouter = require("./users.js");
 const { restoreUser } = require("../../utils/auth.js");
-const spotsRouter = require('../spots.js')
+const spotsRouter = require("./spots.js");
 // Connect restoreUser middleware to the API router
 // If current user session is valid, set req.user to the user in the database
 // If current user session is not valid, set req.user to null
