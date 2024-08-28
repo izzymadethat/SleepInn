a4ef42682e92bedfd520203605c50d52df477d09
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-08-28 10:31:35 -0400

    Add User login backend endpoint

--- backend/routes/sessions.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-  router.get('/',(req,res,next)=>{
-    res.send('sessions routes hosted here')
-  })
-
-
-module.exports = router
