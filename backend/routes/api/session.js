f743121e5ef3ca75a13b72780f4725f81b11ab13
Tyythedeveloper33 <webstertyler81@yahoo.com>
2024-08-30 00:58:48 -0400

    edited get /api/session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -61,13 +61,13 @@ router.get("/", (req, res) => {
   if (user) {
     const safeUser = {
       id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
       email: user.email,
       username: user.username,
     };
-    return res.json({
-      user: safeUser,
-    });
-  } else return res.json({ user: null });
+    return res.status(200).json({ user: safeUser });
+  } else return res.status(200).json({ user: null });
 });
 
 // Logout
