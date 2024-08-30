e7737c9a6c9735ca8d90ac43a4a4b19923c3dbd7
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-08-29 22:27:48 -0400

    add review model

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -1,40 +1,30 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Booking extends Model {
+  class Review extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Booking.belongsTo(models.User, {
-        foreignKey: "userId",
-      });
-      Booking.belongsTo(models.Spot, {
-        foreignKey: "spotId",
-      });
+      // define association here
     }
   }
-  Booking.init(
+  Review.init(
     {
-      spotId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
       userId: {
         type: DataTypes.INTEGER,
         allowNull: false,
       },
-      bookingDate: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
+      spotId: { type: DataTypes.INTEGER, allowNull: false },
+      review: { type: DataTypes.STRING, allowNull: false },
+      stars: { type: DataTypes.FLOAT, allowNull: false, defaultValue: 0 },
     },
     {
       sequelize,
-      modelName: "Booking",
+      modelName: "Review",
     }
   );
-  return Booking;
+  return Review;
 };
