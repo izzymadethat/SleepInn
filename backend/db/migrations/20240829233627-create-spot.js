18d7cd97ca43c03684dff5e663b908b22ee4c5ea
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-08-29 22:32:35 -0400

    add options to check for production schema

--- backend/db/migrations/20240829233627-create-spot.js
+++ backend/db/migrations/20240829233627-create-spot.js
@@ -1,72 +1,80 @@
 "use strict";
 /** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Spots", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      ownerId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: "Users",
-          key: "id",
+    await queryInterface.createTable(
+      "Spots",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        ownerId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: "Users",
+            key: "id",
+          },
+        },
+        address: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        city: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        state: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        country: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        lat: {
+          type: Sequelize.FLOAT,
+        },
+        lng: {
+          type: Sequelize.FLOAT,
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        description: {
+          type: Sequelize.STRING,
+        },
+        price: {
+          type: Sequelize.FLOAT,
+          allowNull: false,
+        },
+        averageRating: {
+          type: Sequelize.FLOAT,
+        },
+        previewImg: {
+          type: Sequelize.STRING,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
         },
       },
-      address: {
-        type: Sequelize.STRING,
-        allowNull:false
-      },
-      city: {
-        type: Sequelize.STRING,
-        allowNull:false
-      },
-      state: {
-        type: Sequelize.STRING,
-        allowNull:false
-      },
-      country: {
-        type: Sequelize.STRING,
-        allowNull:false
-      },
-      lat: {
-        type: Sequelize.FLOAT,
-      },
-      lng: {
-        type: Sequelize.FLOAT,
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull:false
-      },
-      description: {
-        type: Sequelize.STRING,
-      },
-      price: {
-        type: Sequelize.FLOAT,
-        allowNull:false
-      },
-      averageRating: {
-        type: Sequelize.FLOAT,
-      },
-      previewImg: {
-        type: Sequelize.STRING,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
-      },
-    });
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable("Spots");
--- backend/db/migrations/20240830014851-create-booking.js
+++ backend/db/migrations/20240830014851-create-booking.js
@@ -1,48 +1,55 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Bookings', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
+    await queryInterface.createTable(
+      "Bookings",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        spotId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: "Spots",
+            key: "id",
+          },
+        },
+        userId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: "Spots",
+            key: "id",
+          },
+        },
+        bookingDate: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
       },
-      spotId: {
-        type: Sequelize.INTEGER,
-        allowNull:false,
-        references:{
-          model:"Spots",
-          key:"id"
-        }
-
-      },
-      userId: {
-        type: Sequelize.INTEGER,
-        allowNull:false,
-        references:{
-          model:"Spots",
-          key:"id"
-        }
-      },
-      bookingDate: {
-        type: Sequelize.DATE,
-        allowNull:false
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
-      }
-    });
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Bookings');
-  }
+    await queryInterface.dropTable("Bookings");
+  },
 };
