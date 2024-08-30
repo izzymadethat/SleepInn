d039805438a7f056fc88072c4bae34f4299cf25f
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-08-29 21:19:44 -0400

    add create spot

---
+++ backend/db/migrations/20240829233627-create-spot.js
@@ -0,0 +1,72 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Spots", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      ownerId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      state: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      country: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      lat: {
+        type: Sequelize.FLOAT,
+      },
+      lng: {
+        type: Sequelize.FLOAT,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      description: {
+        type: Sequelize.STRING,
+      },
+      price: {
+        type: Sequelize.FLOAT,
+        allowNull:false
+      },
+      averageRating: {
+        type: Sequelize.FLOAT,
+      },
+      previewImg: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Spots");
+  },
+};
