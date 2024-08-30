cd00afb5c25b04ccb0d422987729f2c740aadbae
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-08-29 23:48:45 -0400

    add create image model

---
+++ backend/db/migrations/20240830034347-create-image.js
@@ -0,0 +1,47 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Images", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      url: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      spotId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Spots",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+      },
+      reviewId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Reviews",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Images");
+  },
+};
