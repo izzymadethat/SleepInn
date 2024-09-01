af9d20b4e19096405440251a666fe48f14f28c95
Tyythedeveloper33 <webstertyler81@yahoo.com>
2024-09-01 17:30:34 -0400

    added route for getting spot by spot id while including owner added firstname, lastname to user model

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,9 +1,40 @@
 const express = require("express");
 const router = express.Router();
 const { Spot } = require("../../db/models");
+const { User } = require("../../db/models");
 const {requireAuth} = require("../../utils/auth");
+const Sequelize = require('sequelize');
 
-router.get('/:ownerId', requireAuth ,async(req,res, next) =>{
+
+router.get('/:spotId', async(req,res, next) =>{
+  const spotId = Number(req.params.spotId)
+   console.log(spotId)
+
+
+
+  try {
+    const spot = await Spot.findOne({
+      where: { id:spotId },
+      include: [{
+        model: User,
+        where: { id: Sequelize.col('Spot.ownerId') },// Match User's id to Spot's ownerId
+        attributes: ['id', 'firstName', 'lastName']
+      }]
+    });
+   if (!spot) {
+    //spot not found
+    return res.status(404).json({ message: "Spot couldn't be found" });
+  }
+
+  res.json(spot);
+  } catch(e){
+    res.status(500)
+    console.error(e);
+    next(e);
+  }
+})
+
+router.get('/owner/:ownerId', requireAuth , async(req,res, next) =>{
   const ownerId = Number(req.params.ownerId)
   try{
     const allSpots = await Spot.findAll({
