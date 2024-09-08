cb970b1b9043ac324b3ebd1692b8504ebddbea3b
Isaiah V. <isaiah.vickers@outlook.com>
2024-09-08 05:27:36 -0400

    add reviews router to main api routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,7 @@ const usersRouter = require("./users.js");
 const { restoreUser } = require("../../utils/auth.js");
 const spotsRouter = require("./spots.js");
 const bookingRouter = require("./booking.js");
+const reviewsRouter = require("./reviews.js");
 // Connect restoreUser middleware to the API router
 // If current user session is valid, set req.user to the user in the database
 // If current user session is not valid, set req.user to null
@@ -14,7 +15,7 @@ router.use("/spots", spotsRouter);
 
 router.use("/users", usersRouter);
 router.use("/booking", bookingRouter);
-
+router.use("/reviews", reviewsRouter);
 
 router.post("/test", (req, res) => {
   res.json({ requestBody: req.body });
