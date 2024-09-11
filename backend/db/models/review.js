4155bf51f60aa23e5aff5265fb58b97348111dcd
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-09-11 13:38:03 -0400

    test review model

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -8,9 +8,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Review.belongsTo(models.User, {
-        foreignKey: "userId",
-      });
+      // Review.belongsTo(models.User, {
+      //   foreignKey: "userId",
+      // });
 
       Review.belongsTo(models.Spot, {
         foreignKey: "spotId",
