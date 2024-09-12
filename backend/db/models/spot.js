dff098b815b3e5020305d7fc199816cf7a0dcdac
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-09-11 20:24:06 -0400

    modify spot model associations

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -16,6 +16,14 @@ module.exports = (sequelize, DataTypes) => {
       Spot.hasMany(models.SpotImage, {
         foreignKey: "spotId",
       });
+
+      Spot.hasMany(models.Review, {
+        foreignKey: "spotId",
+      });
+
+      Spot.hasMany(models.Booking, {
+        foreignKey: "spotId",
+      });
     }
   }
   Spot.init(
