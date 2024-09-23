719f812e55e53cab4e9ae72c4dac6009bffd74f1
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-09-23 11:49:08 -0400

    update booking-userId association

--- backend/db/migrations/20240830014851-create-booking.js
+++ backend/db/migrations/20240830014851-create-booking.js
@@ -28,7 +28,7 @@ module.exports = {
           type: Sequelize.INTEGER,
           allowNull: false,
           references: {
-            model: "Spots",
+            model: "Users",
             key: "id",
           },
           onDelete: "CASCADE",
