0701d0f840e54f96c762795363fcfca6b3477369
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-10-11 15:04:53 -0400

    built styles for login modal

--- frontend/src/components/LoginFormModal/LoginFormModal.jsx
+++ frontend/src/components/LoginFormModal/LoginFormModal.jsx
@@ -4,11 +4,13 @@ import * as sessionActions from "../../store/session";
 import { useDispatch } from "react-redux";
 import "./LoginFormModal.css";
 import { useModal } from "../../context/Modal";
+import { FaEye, FaEyeSlash } from "react-icons/fa6";
 
 const LoginFormModal = () => {
   const dispatch = useDispatch();
   const [credential, setCredential] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [errors, setErrors] = useState({});
   const { closeModal } = useModal();
 
@@ -19,34 +21,69 @@ const LoginFormModal = () => {
       .then(closeModal)
       .catch(async (res) => {
         const data = await res.json();
-        if (data?.errors) setErrors(data.errors);
+        if (data?.errors) {
+          setErrors(data.errors);
+          setPassword("");
+        }
       });
   };
 
   return (
     <>
-      <h1>Login</h1>
+      <h1 className="login-form__title">Login</h1>
       <form className="login-form" onSubmit={handleSubmit}>
-        <label className="login-form__input">
-          Username or Email
+        <div className="login-form__input-container">
           <input
             type="text"
             value={credential}
             onChange={(e) => setCredential(e.target.value)}
             required
+            id="username-or-email"
           />
-        </label>
-        <label>
-          Password
+          <label
+            htmlFor="username-or-email"
+            className={`label ${credential ? "active" : ""}`}
+          >
+            Username or Email
+          </label>
+        </div>
+        <div className="login-form__input-container">
           <input
-            type="password"
+            type={showPassword ? "text" : "password"}
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
+            id="password"
           />
-        </label>
-        {errors.credential}
-        <button type="submit">Log In</button>
+          <label
+            htmlFor="password"
+            className={`label password-label ${password ? "active" : ""}`}
+          >
+            Password
+          </label>
+          {errors.credential && (
+            <p className="login-form__error">{errors.credential}</p>
+          )}
+          <button
+            type="button"
+            className="show-password-btn"
+            onClick={() => setShowPassword(!showPassword)}
+            aria-label={showPassword ? "Hide Password" : "Show Password"}
+          >
+            {showPassword ? <FaEyeSlash /> : <FaEye />}
+          </button>
+        </div>
+
+        <button
+          style={{
+            opacity: credential && password ? 1 : 0.5,
+            cursor: !credential || !password ? "not-allowed" : "pointer"
+          }}
+          type="submit"
+          disabled={!credential || !password}
+        >
+          Log In
+        </button>
       </form>
     </>
   );
