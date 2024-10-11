19b2d6025e7bb0f27795e0d5377798700ebd7e6a
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-10-11 00:45:38 -0400

    setup backend to get ready for frontend development

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -21,8 +21,4 @@ router.use("/reviews", reviewsRouter);
 router.use("/spot-images", spotImagesRouter);
 router.use("/review-images", reviewImagesRouter);
 
-router.post("/test", (req, res) => {
-  res.json({ requestBody: req.body });
-});
-
 module.exports = router;
